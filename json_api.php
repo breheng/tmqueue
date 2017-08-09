bd2b6950dcfc0dcab42e423a046fd77a83505e55
breheng <30854872+breheng@users.noreply.github.com>
2017-08-09 15:23:47 +0800

    Update json_api.php

--- json_api.php
+++ json_api.php
@@ -1,7 +1,8 @@
-<?
-
-$json = file_get_contents('url_here');
-$obj = json_decode($json);
-echo $obj->access_token;
-
+<?php
+$json_url = "http://itnthackathon.bweas.tm.com.my/api/getTMPointInfo";
+$json = file_get_contents($json_url);
+$data = json_decode($json, TRUE);
+echo "<pre>";
+print_r($data);
+echo "</pre>";
 ?>
