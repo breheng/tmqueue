fb675e6906d0ff42ec127e1811fe26004830705d
breheng <30854872+breheng@users.noreply.github.com>
2017-08-09 20:57:46 +0800

    Update json_api.php

--- json_api.php
+++ json_api.php
@@ -5,9 +5,6 @@ include 'httpful.phar';
 $json_url = "http://itnthackathon.bweas.tm.com.my/api/getTMPointInfo";
 $json = file_get_contents($json_url);
 $data = json_decode(callAPI($json_url));
-echo "<pre>";
-print_r($data);
-echo "</pre>";
 echo convertJSON($data->{'getTMPointInfo'}->{'records'}[2]);
 
 function convertJSON($array){
